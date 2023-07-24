0a9f82784cfd379b30c36a3d000cc5d095e95974
bynico1 <nicofoque0205@gmail.com>
2023-07-24 17:06:35 -0500

    added range slider to make grid dynamic

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const gridContainer = document.querySelector(".grid-container");
+const rangeValue = document.querySelector(".input-value");
+const rangeSlider = document.querySelector("#myRange");
+
+rangeValue.innerText = `${rangeSlider.value} x ${rangeSlider.value}`;
+rangeSlider.oninput = sliderChange;
+
+function sliderChange() {
+  makeRows(rangeSlider.value, rangeSlider.value);
+  rangeValue.innerText = `${rangeSlider.value} x ${rangeSlider.value}`;
+}
+
+function randomColor() {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  this.style.backgroundColor = randomColor;
+}
 
 function makeRows(rows, cols) {
   gridContainer.style.setProperty("--grid-rows", rows);
   gridContainer.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
+  for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
+    cell.addEventListener("mouseover", randomColor);
     gridContainer.appendChild(cell).className = "grid-item";
   }
 }
-
-makeRows(16, 16);
+makeRows(rangeSlider.value, rangeSlider.value);
