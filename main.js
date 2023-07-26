61d05a92d1bcbaf7ffb4623ed5db6aa6a233c99b
bynico1 <nicofoque0205@gmail.com>
2023-07-26 16:06:11 -0500

    random and custom color buttons features and better styles

--- main.js
+++ main.js
@@ -3,8 +3,22 @@ const clearButton = document.querySelector(".clear-button");
 const rangeValue = document.querySelector(".input-value");
 const rangeSlider = document.querySelector("#myRange");
 
+const randomColor = document.querySelector(".random-color");
+const customColor = document.querySelector("#customColor");
+
 rangeValue.innerText = `${rangeSlider.value} x ${rangeSlider.value}`;
 rangeSlider.oninput = sliderChange;
+
+randomColor.addEventListener("click", () => (mode = "random"));
+customColor.addEventListener("input", customColorF);
+
+let mode = "random";
+let globalColor;
+
+function customColorF(e) {
+  mode = "custom";
+  globalColor = e.target.value;
+}
 
 function clear() {
   gridContainer.innerHTML = "";
@@ -16,9 +30,13 @@ function sliderChange() {
   rangeValue.innerText = `${rangeSlider.value} x ${rangeSlider.value}`;
 }
 
-function randomColor() {
+function colorChange() {
   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  this.style.backgroundColor = randomColor;
+  if (mode === "random") {
+    this.style.backgroundColor = randomColor;
+  } else {
+    this.style.backgroundColor = globalColor;
+  }
 }
 
 function makeRows(rows, cols) {
@@ -26,7 +44,7 @@ function makeRows(rows, cols) {
   gridContainer.style.setProperty("--grid-cols", cols);
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.addEventListener("mouseover", randomColor);
+    cell.addEventListener("mouseover", colorChange);
     gridContainer.appendChild(cell).className = "grid-item";
   }
 }
