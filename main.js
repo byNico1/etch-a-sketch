9843f874d2305ab0bbaeefe579f7d3d2970e6043
bynico1 <nicofoque0205@gmail.com>
2023-07-26 16:18:42 -0500

    onclick functionality

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function makeRows(rows, cols) {
   gridContainer.style.setProperty("--grid-cols", cols);
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.addEventListener("mouseover", colorChange);
+    cell.addEventListener("click", colorChange);
     gridContainer.appendChild(cell).className = "grid-item";
   }
 }
